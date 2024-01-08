2e0ac6f8a2e2eba746902d5b7044d02485b91717
Sanny Nguyen Hung <sanny.nguyen@digitalservice.bund.de>
2024-01-08 21:36:08 +0100

    chore: some font adjustments

--- src/components/blocks/elements/content.tsx
+++ src/components/blocks/elements/content.tsx
@@ -3,6 +3,17 @@ import { RichText } from 'alinea/ui'
 
 export function Content({content} : {content: TextDoc<{}>}) {
     return (<RichText
+        p={<p className="text-lg" />}
+
+        h1={<h1 className="ds-heading-01-reg" />}
+        h2={<h1 className="ds-heading-02-reg" />}
+        h3={<h1 className="ds-heading-03-reg" />}
+        h4={<h1 className="ds-heading-04-reg" />}
+        h5={<h1 className="ds-heading-05-reg" />}
+        h6={<h1 className="ds-heading-06-reg" />}
+        a={<a className="text-link" />}
+        
+
         doc={content}
     />);
 }
