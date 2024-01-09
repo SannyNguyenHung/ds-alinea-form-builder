4acb847bd2888da11694e4518e4deffa784752c9
Sanny Nguyen Hung <sanny.nguyen@digitalservice.bund.de>
2024-01-10 00:17:05 +0100

    refactor: fix hydration error &

--- src/components/page.tsx
+++ src/components/page.tsx
@@ -3,6 +3,8 @@ import { ContentBlock, Content } from "./blocks/content";
 import { HeaderBlock, Header } from "./blocks/header";
 import { HeroBlock, Hero } from "./blocks/hero";
 import { LogoBanner, LogoBannerBlock } from "./blocks/logoBanner";
+import { CallToAction, CallToActionBlock } from "./blocks/callToAction";
+import { v4 as uuidv4 } from "uuid";
 
 export const PageSchema = alinea.type('Page', {
     title: alinea.text('Title'),
@@ -12,7 +14,8 @@ export const PageSchema = alinea.type('Page', {
         Text: ContentBlock,
         Header: HeaderBlock,
         Hero: HeroBlock,
-        LogoBanner: LogoBannerBlock
+        LogoBanner: LogoBannerBlock,
+        CallToAction: CallToActionBlock
       })
     })
   });
@@ -25,13 +28,15 @@ export default function Page({page} : {page: Page}) {
             {page.blocks.map(block => {
                 switch (block.type) {
                     case 'Text':
-                        return <Content block={block} />
+                        return <Content key={uuidv4()} block={block} />
                     case 'Header':
-                        return <Header block={block} />
+                        return <Header key={uuidv4()} block={block} />
                     case 'Hero':
-                        return <Hero block={block} />
+                        return <Hero key={uuidv4()} block={block} />
                     case 'LogoBanner':
-                        return <LogoBanner block={block} />
+                        return <LogoBanner key={uuidv4()} block={block} />
+                    case 'CallToAction':
+                        return <CallToAction key={uuidv4()} block={block} />
                     default:
                         return null
                 }
